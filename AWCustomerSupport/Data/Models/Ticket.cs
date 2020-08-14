8cc63bbe1520e8d44f52db33aa731d0a3ac3577e
Alo Pullmann <alpull@ttu.ee>
2020-08-14 03:53:46 +0300

    Added more validation for Ticket properties, TimeToDeadline rollback

--- AWCustomerSupport/Data/Models/Ticket.cs
+++ AWCustomerSupport/Data/Models/Ticket.cs
@@ -5,14 +5,24 @@ namespace AWCustomerSupport.Data.Models {
 
     public class Ticket {
 
+        private const string NAME_TOO_SHORT = "Name must be at least 3 characters.";
+        private const string NAME_TOO_LONG = "Name must be a maximum of 100 characters.";
+
+        private const string DESCRIPTION_TOO_SHORT = "Description must be at least 10 characters.";
+        private const string DESCRIPTION_TOO_LONG = "Name must be a maximum of 300 characters.";
+
         public const string DATE_FORMAT = "dd/MM/yyyy HH:mm";
 
         public int Id { get; private set; }
 
         [Required]
+        [MinLength(3, ErrorMessage = NAME_TOO_SHORT),
+         MaxLength(100, ErrorMessage = NAME_TOO_LONG)]
         public string Name { get; set; }
 
         [Required]
+        [MinLength(10, ErrorMessage = DESCRIPTION_TOO_SHORT),
+         MaxLength(300, ErrorMessage = DESCRIPTION_TOO_LONG)]
         public string Description { get; set; }
 
         [Display(Name = "Entry")]
@@ -21,7 +31,7 @@ namespace AWCustomerSupport.Data.Models {
         [ValidateDateRange]
         public DateTime Deadline { get; set; }
 
-        public TimeSpan TimeToDeadline => Deadline - EntryDate;
+        public TimeSpan TimeToDeadline => Deadline - DateTime.Now;
 
     }
 
